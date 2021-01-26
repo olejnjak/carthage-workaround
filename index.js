c5867cf72f2f00579eaccbb901bc42db5ec078a3
Jakub Olejnik <olejnjak@gmail.com>
2021-01-26 02:51:42 +0100

    🔧 Update script to use exec package

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { execSync } = require("child_process");
+const core = require('@actions/core')
+const exec = require('@actions/exec')
 
-try {
-    const action = core.getInput('action');
-    const ref = github.context.ref;
+async function main() {
+    const action = core.getInput('action')
     
-    execSync(`curl https://raw.githubusercontent.com/olejnjak/carthage-workaround/${ref}/carthage.sh > carthage.sh`);
-    execSync(`chmod +x carthage.sh`);
-    execSync(`./carthage.sh ${action}`);
-} catch (error) {
-    core.setFailed(error.message);
+    await exec.exec(`${__dirname}/carthage.sh ${action}`)
 }
+
+main().catch(err => core.setFailed(err.message))
