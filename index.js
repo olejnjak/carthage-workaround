71b9d60133415d555299868c4bde9fa1166f8e77
Jakub Olejnik <olejnjak@gmail.com>
2021-01-26 01:51:05 +0100

    🙈 Update gitignore

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const { execSync } = require("child_process");
 
 try {
-    const action = core.getInput('action')
+    const action = core.getInput('action');
+    const ref = github.context.ref;
+    
+    execSync(`curl https://raw.githubusercontent.com/olejnjak/carthage-workaround/${ref}/carthage.sh > carthage.sh`);
+    execSync(`chmod +x carthage.sh`);
     execSync(`./carthage.sh ${action}`);
 } catch (error) {
     core.setFailed(error.message);
