f8a48fd274a1ba2023464e11fb92b96ace4bdd99
Jakub Olejnik <olejnjak@gmail.com>
2021-02-17 19:12:03 +0100

    🔧 Run workaround script by default only if building using Xcode 12

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Carthage workaround
-GitHub action performing workaround so Carthage is capable of building dependencies without M1 arch
 
+GitHub action performing workaround so Carthage is capable of building dependencies without M1 arch
 
 ## Usage
 
@@ -20,4 +20,24 @@ jobs:
       - uses: olejnjak/carthage-workaround@v1
         with:
           action: bootstrap --platform iOS --cache-builds
+```
+
+By default the workaround script will be used only if you are building using Xcode 12.0 and higher and also if you are not using the `--use-xcframeworks` parameter. 
+If you still want to use the workaround script, you can use the `force` input parameter.
+
+```
+name: Build
+
+on: [pull_request, push]
+
+jobs:
+  tests:
+    name: Build
+    runs-on: macos-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: olejnjak/carthage-workaround@v1
+        with:
+          action: bootstrap --platform iOS --cache-builds
+          force: true
 ```
--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const core = require('@actions/core')
 const exec = require('@actions/exec')
 
+function execForResult(cmd, args) {
+    var myOutput = ""
+    var myError = ""
+
+    const options = {};
+    options.listeners = {
+    stdout: data => {
+        myOutput += data.toString();
+    },
+    stderr: data => {
+        myError += data.toString();
+    }
+    };
+
+    await exec.exec(cmd, args, options)
+
+    return myOutput
+}
+
+function runCarthageWorkaround() {
+    await exec.exec(`${__dirname}/carthage.sh ${action}`)
+}
+
 async function main() {
     const action = core.getInput('action')
+    const force = core.getInput('force')
+
+    if (force) {
+        runCarthageWorkaround()
+        return
+    }
+
+    const xcodeVersion = parseFloat(execForResult("xcodebuild -version | head -n1 | cut -f2 -d' '", []))
     
-    await exec.exec(`${__dirname}/carthage.sh ${action}`)
+    if (xcodeVersion >= 12) {
+        runCarthageWorkaround()
+    } else {
+        await exec.exec(`carthage ${action}`)
+    }
 }
 
 main().catch(err => core.setFailed(err.message))
